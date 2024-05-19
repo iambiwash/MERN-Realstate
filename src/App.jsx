876d998c7a8f686698e22e095f8ac9524d56bae1
iambiwash <iambiwash@gmail.com>
2024-05-19 13:12:59 -0500

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 export default function App() {
-  return <h1 className="text-red-600">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
