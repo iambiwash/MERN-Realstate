58f5e85c1a17621123f40ca6d7e9d3a8614e3d8b
iambiwash <iambiwash@gmail.com>
2024-05-20 15:33:55 -0500

    signin api route created

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user_model.js";
 import bcryptjs from "bcryptjs";
-// import errorHandler from "../utils/error.js";
+import errorhandler from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,6 +10,27 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created Successfully !");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    //check if valid user on database
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorhandler(404, "User not Found"));
+    //compare password with one inside db
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorhandler(401, "Wrong Credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    //avoid sharing password to user even if its hashed. so get everything beside password of a user.
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true }) //"expires: new Date(Date.now() + 24*60*60*1000" --> add this to provide expiration time if needed..
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
