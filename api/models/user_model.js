7b0f1eef939b3e5e372fab40d16f3b0d6a75b76b
iambiwash <iambiwash@gmail.com>
2024-05-23 12:06:49 -0500

    update header and privatize profile page

--- api/models/user_model.js
+++ api/models/user_model.js
@@ -19,7 +19,7 @@ const userSchema = new Mongoose.Schema(
     avatar: {
       type: String,
       default:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonduck.com%2Ficons%2F180867%2Fprofile-circle&psig=AOvVaw1wVENpDs4it4WmGdrhxsfz&ust=1716516967748000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOjDqqjaooYDFQAAAAAdAAAAABAE",
+        "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg",
     },
   },
   { timestamps: true }
