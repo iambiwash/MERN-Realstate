c89b5ad7756cbcda60dc5478eed84688ea8814f9
iambiwash <iambiwash@gmail.com>
2024-05-25 18:36:12 -0500

    user listing deletion functionality added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -144,6 +144,24 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -264,7 +282,10 @@ export default function Profile() {
                 <p>{listing.name}</p>
               </Link>
               <div className="flex flex-col">
-                <button className="text-red-700 hover:opacity-75">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 hover:opacity-75"
+                >
                   Delete
                 </button>
                 <button className="text-green-700 hover:opacity-75">
