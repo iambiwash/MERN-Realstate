863562f05ae52ccaa910ae2a7f566d7ac99787cd
iambiwash <iambiwash@gmail.com>
2024-06-07 23:03:02 -0500

    error solved

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -93,7 +93,7 @@ export default function Listing() {
             <p className="text-2xl font-semibold">
               {listing.name} - ${" "}
               {listing.offer
-                ? listing.discountPrice.toLocaleString("en-US")
+                ? listing.discountedPrice.toLocaleString("en-US")
                 : listing.regularPrice.toLocaleString("en-US")}
               {listing.type === "rent" && " / month"}
             </p>
